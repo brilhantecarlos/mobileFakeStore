895bbad1b61fcfdfd8c6aeae0bfea9371b7bacd9
Carlos Santos <carlos.santos@dreamxp.com.br>
2025-06-14 21:51:22 -0300

    feat: melhorias de design e criação de menu com categorias

--- mobileFakeStore/src/app/tab1/tab1.page.ts
+++ mobileFakeStore/src/app/tab1/tab1.page.ts
@@ -11,6 +11,9 @@ export class Tab1Page {
 
   products: any[] = [];
   loading: boolean = true;
+  allProducts: any[] = [];
+  categories: string[] = [];
+  selectedCategory: string = 'Todas';
   error: string | null = null;
 
   constructor(private fakeStoreService: FakeStoreService) {}
@@ -27,5 +30,50 @@ export class Tab1Page {
         this.loading = false;
       }
     });
+  }
+
+  getCategories() {
+    this.fakeStoreService.getAllCategories().subscribe({
+      next: (data) => {
+        this.categories = data;
+        this.categories.unshift('Todas');
+      }
+    });
+  }
+
+  getProductsByCategory(category?: string) {
+    this.loading = true;
+
+    if (category && category !== 'Todas') {
+      this.fakeStoreService.getProductsByCategory(category).subscribe({
+        next: (data) => {
+          this.products = data;
+          this.loading = false;
+        },
+        error: () => {
+          this.error = 'Erro ao carregar produtos da categoria';
+          console.error('Erro ao carregar produtos da categoria');
+          this.loading = false;
+        }
+      });
+    }
+    else {
+      this.fakeStoreService.getProducts().subscribe({
+        next: (data) => {
+          this.products = data;
+          this.loading = false;
+        },
+        error: () => {
+          this.error = 'Erro ao carregar produtos';
+          console.error('Erro ao carregar produtos');
+          this.loading = false;
+        }
+      });
+    }
+  }
+
+  filterByCategory(category: string) {
+    this.selectedCategory = category;
+    this.getProductsByCategory(category);
   }
 }
